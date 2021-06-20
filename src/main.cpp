d5eae3da625009fe05c76090fee11475f5a9dfd5
randomgit3000 <60452010+randomgit3000@users.noreply.github.com>
2021-06-20 17:38:36 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,73 +9,81 @@ D7 - ECHO, ultrasonic sensor SR04T
 #define BLYNK_PRINT Serial
 #include <BlynkSimpleEsp8266.h>
 #include <ESP8266WiFi.h>
-#include <NewPing.h>
-#include <MedianFilter.h>  // Filter OFF/ON
 
-const float Pi = 3.14;
-const float Radius = 0.4;   // Радиус кольца колодца
-const float Depth = 5.24;   // Глубина колодца
-const float Rod = 1.52;     // Длина штанги, на конце с ультазвуковым датчиком
-float measuring;            // Расстояние, измеряется от конца штанги (датчика) до воды в см.
-float distance;             // Расстояние, расчитывается от края колодца до воды в м.
-float level;                // Уровень воды в м.
-float capacity;             // Объем воды в м3
-unsigned long last_time;
-unsigned long last_time_2;
+//#define TRIGGER_PIN  12     // D6 Arduino pin tied to trigger pin on the ultrasonic sensor.
+//#define ECHO_PIN     13     // D7 Arduino pin tied to echo pin on the ultrasonic sensor.
 
-#define TRIGGER_PIN  12     // D6 Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     13     // D7 Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 450    // Maximum sensor distance is rated at 400-500cm.
-//#define SEALEVELPRESSURE_HPA (1013.25) // 760мм.рт.ст. = 1013.25 гПа (hPa)
-
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-MedianFilter filter(31,0); // Filter OFF/ON
+#define TRIGGER_PIN 13 //!!!!!!!!!!!!
+#define ECHO_PIN 12 //!!!!!!!!!!!!
+#define USONIC_DIV 58.0
+#define MEASURE_SAMPLE_DELAY 5
+#define MEASURE_SAMPLES 20
+#define MEASURE_DELAY 2
 
 char auth[] = "tQSG6TYMuj33_0OrdRxhvxIkmiNAxrbZ";
 char ssid[] = "";
 char pass[] = "";
 
-void setup() {
+long distance_long = 111;
+long distance_single = 222;
+
+void setup()
+{
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);  // Запуск Blynk
+  Blynk.begin(auth, ssid, pass); // Запуск Blynk
+  Serial.println("setup");
+  pinMode(TRIGGER_PIN, OUTPUT); // Initializing Trigger Output and Echo Input
+  pinMode(ECHO_PIN, INPUT);
+  digitalWrite(TRIGGER_PIN, LOW); // Reset the trigger pin and wait a half a second
+  delayMicroseconds(500);
+}
+
+long singleMeasurement()
+{
+
+  long duration = 0;               // Measure: Put up Trigger...
+  digitalWrite(TRIGGER_PIN, HIGH); // Wait for 11 µs ...
+  delayMicroseconds(11);           // Put the trigger down ...
+  digitalWrite(TRIGGER_PIN, LOW);  // Wait for the echo ...
+  duration = pulseIn(ECHO_PIN, HIGH);
+
+  return (long)(((float)duration / USONIC_DIV) * 10.0);
+}
+
+long measure()
+{
+  long measureSum = 0;
+  for (int i = 0; i < MEASURE_SAMPLES; i++)
+  {
+    delay(MEASURE_SAMPLE_DELAY);
+    measureSum += singleMeasurement();
+  }
+  return measureSum / MEASURE_SAMPLES;
+}
+
+
+
+void sendMeasure()
+{
+  Serial.print("Distance logng: ");
+  Serial.print(distance_long + 22);
+  Serial.println();
+  Serial.print("Distance singleMeasurement: ");
+  Serial.println(distance_single + 22);
   Serial.println();
 
-  Blynk.setProperty(V7, "label", "Глубина");
-  Blynk.setProperty(V13, "label", "Уровень");
-  Blynk.setProperty(V15, "label", "Объем");
+  Blynk.virtualWrite(V1, distance_long); 
+  Blynk.virtualWrite(V2, distance_single); 
 }
 
-void sendMeasure() {
-  
-  Serial.println("    До воды: " + String(measuring) + " см.");
-  Serial.println ();
-  Blynk.virtualWrite(V7, Depth);      // Глубина колодца
-  Blynk.virtualWrite(V13,level);      // Уровень воды
-  Blynk.virtualWrite(V15,capacity);   // Объем воды
-  Blynk.virtualWrite(V12,measuring);  // Измеренное расстояние
-}
-
-
-void loop() {
+void loop()
+{
   Blynk.run();
 
-  if (millis() - last_time_2 > 50) {    // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-    last_time_2 = millis();
+  delay(MEASURE_DELAY);
+  distance_long = measure();
+  distance_single = singleMeasurement();
 
-    unsigned int oo,uS = sonar.ping(); // Send ping, get ping time in microseconds (uS). Filter ON //-OFF
-    Serial.println("    oo " + oo);
-    Serial.println("    uS " + uS); 
-    filter.in(uS);
-    oo = filter.out();
-
-    measuring = (oo / US_ROUNDTRIP_CM);
-    distance = measuring / 100 + Rod;
-    level = Depth - distance;
-    capacity = Pi * Radius * Radius * level;
-  }
-
-  if (millis() - last_time > 1*1000) {           // Интервал времени выдачи измерений  в мсек
-    last_time = millis();
-    sendMeasure();
-  }
+  sendMeasure();
 }
+
